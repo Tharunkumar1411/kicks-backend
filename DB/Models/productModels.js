78a4cda2c830772ca1048991094555e0a30e464f
Tharunkumar G <tharung1411@gmail.com>
2025-12-31 21:49:27 +0530

    fix

--- DB/Models/productModels.js
+++ DB/Models/productModels.js
@@ -1,55 +1,33 @@
 import mongoose from "mongoose";
-const UserSchema  = require("../schema/userShcema");
-
+import { UserSchema } from "../Schema/userSchema.js";
 
 const HomeSchema = new mongoose.Schema({}, { strict: false });
-
 const ProductSchema = new mongoose.Schema({}, { strict: false });
 
-const User = mongoose.model("User", UserSchema, 'user');
+const User = mongoose.model("User", UserSchema, "user");
+const Home = mongoose.model("Home", HomeSchema, "home");
+const Product = mongoose.model("Product", ProductSchema, "product");
 
-const Home = mongoose.model("Home", HomeSchema, 'home');
-
-const Product = mongoose.model("Product", ProductSchema, 'product');
-
-const workflowSchema = new mongoose.Schema({
-    flowId: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    nodes: {
-      type: Array,
-      default: []
-    },
-    edges: {
-      type: Array,
-      default: []
-    },
-    flowName: {
-      type: String,
-      required: true
-    },
-    editedOn: {
-      type: String,
-      required: false
-    },
-    name: {
-      type: String,
-      required: false
-    },
+const workflowSchema = new mongoose.Schema(
+  {
+    flowId: { type: String, required: true, unique: true },
+    nodes: { type: Array, default: [] },
+    edges: { type: Array, default: [] },
+    flowName: { type: String, required: true },
+    editedOn: { type: String },
+    name: { type: String },
     description: {
       type: String,
-      default: "Some description here regarding the flow.."
+      default: "Some description here regarding the flow..",
     },
     apiConfig: {
       type: mongoose.Schema.Types.Mixed,
-      default: null
-    }
-  }, {
-    timestamps: true
-});
-  
-const WorkFlow = mongoose.model('WorkFlow', workflowSchema);
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = { User, Home, Product, WorkFlow }
+const WorkFlow = mongoose.model("WorkFlow", workflowSchema);
+
+export { User, Home, Product, WorkFlow };
